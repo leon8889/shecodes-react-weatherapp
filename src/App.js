8aaef56a57f6f7ee8c981778f05f0fea9b0c9494
leon8889 <anna.kuhn89@gmx.de>
2023-05-22 09:13:09 +0200

    removed unused code

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ import "./App.css";
 
 function App() {
 	let city = "Paris";
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
 				<h1>Hello World from {city}!</h1>
 				<h2>This is my first React App</h2>
 				<Weather city={city} />
 				<a href="shecodes.io" target="_blank">
 					SheCodes
-        </a>
-				<img src="logo192.png" alt="test" />
-      </header>
-    </div>
-  );
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
